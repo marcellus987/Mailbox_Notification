0e02ac1c7ec3547405b9c05530cdbfa8b548c655
Marcellus <marcellus.sacramento@gmail.com>
2026-07-12 01:26:09 -0700

    Updated to test minor driver update.

--- mailbox_notification_receiver/Src/main.c
+++ mailbox_notification_receiver/Src/main.c
@@ -5,6 +5,7 @@
 #include "rtc_driver.h"
 #include "rtc_module_driver.h"
 #include "ff.h"
+#include "uart_driver.h"
 
 
 #define DEBUG_MODE__
@@ -17,10 +18,16 @@
  *
  * */
 
-void checkFRESULT(FRESULT res);
-FRESULT makeLog(FIL* file, UINT* bw, uint8_t mail_status);
 
+/**** Global variables START. *****/
+volatile uint8_t run = 1;
+
+/**** Global variables END. *****/
+
+void checkFRESULT(FRESULT res);
+void configureButton(void);
 uint8_t checkHeaderPresent(char* target, char* header, uint32_t headerLen);
+FRESULT makeLog(FIL* file, UINT* bw, uint8_t mail_status);
 
 int main(void) {
 	FATFS fs;   /* File system. */
@@ -28,50 +35,54 @@ int main(void) {
 	FRESULT res; /* Result from file function calls. */
 	UINT br; /* Bytes read from file. */
 	UINT bw; /* Bytes written to file. */
-	uint8_t mail_status = 0;
-	uint32_t fileSize; /* Though f_size returns uint64_t, it may be unrealistic
+	uint8_t mail_status = 1;
+	uint32_t fileSize; /* Though f_size() returns uint64_t, it may be unrealistic
 	 	 	 	 	 	  for a CSV file to be that large. So, only the lower 32-bit
 	 	 	 	 	 	  will be used for now. */
 
+	char header[] = "Date: [mm-dd-yyyy], Time (24-hour format): [hh-mm-ss], Status:\r\n";
+	uint32_t headerLen = strlen(header);
+
+	char testRead[headerLen + 1]; /* Contains what is read from file. +1 for null char to be added.
+	 	 	                       * Though, it may not be necessary since memcmp() would use strlen(header)
+	 	 	                       * which returns size not including the null char.*/
+
 #ifdef DEBUG_MODE__
+//	uart_init(1);
+//	setBaudrate(115200);
 	/* For user-LED debug indication. */
 	init_debug_mode();
+	configureButton();
+	received_led_init();
 #endif
+
+	rtc_module_init();
 
 	/* Mount drive. */
     res = f_mount(&fs, "",0);
 	checkFRESULT(res);
 
 	/* Open log file.*/
-	res = f_open(&file, "log.csv", FA_WRITE | FA_READ | FA_OPEN_APPEND);
+	res = f_open(&file, "mail_log.csv", FA_WRITE | FA_READ | FA_OPEN_APPEND);
 	checkFRESULT(res);
 
 	/* Get lower 32-bit of the returned value of f_size(). */
 	fileSize = f_size(&file) & 0xFFFFFFFFU;
 
-
-	char header[] = "Timestamp: [mm-dd-yyyy] [hh-mm-ss], Status:\r\n";
-	uint32_t headerLen = strlen(header);
-
-	/* Contains what is read from file. +1 for null char to be added.
-	 * Though, it may not be necessary since memcmp() would use strlen(header)
-	 * which returns size not including the null char.*/
-	char testRead[headerLen + 1];
-
 	/* Set cursor to beginning. Then perform a read. */
 	f_lseek(&file, 0);
 	res = f_read(&file, testRead, headerLen, &br);
-
 	checkFRESULT(res);
 
-	if(br != headerLen) {
-		debug_led_on();
-	}
+//	if(br != headerLen) {
+//		debug_led_on();
+////		printf("br != headerLen...\r\n");
+//	}
 
 	/* Add null char to end of the string read from file. */
 	testRead[headerLen] = '\0';
 
-	/* Move back the cursor to EOF. To prepare for possible write operations. */
+	/* Move back the cursor to EOF. To prepare for possible file write operations. */
 	f_lseek(&file, fileSize);
 
 	/* Check if header read from file matches the required header.
@@ -90,32 +101,34 @@ int main(void) {
 		}
 	}
 
+	while(run) {
+//		receive_notification(&mail_status);
 
+		if(mail_status == MAIL_PRESENT) {
+		//		debug_led_on();
+			received_led_on();
+		}
+		else {
+		//		debug_led_off();
+			received_led_off();
+		}
 
+		res = makeLog(&file, &bw, mail_status);
+		checkFRESULT(res);
 
-	while(1) {
-//		receiver_notification(&mail_status);
-//
-//		if(mail_status == MAIL_PRESENT) {
-//		//		debug_led_on();
-//			received_led_on();
-//			res = makeLog(&file, &bw, mail_status);
-//		}
-//		else {
-//		//		debug_led_off();
-//			received_led_off();
-//			res = makeLog(&file, &bw, mail_status);
-//		}
-//
-//		res = makeLog(&file, &bw, mail_status);
-//
-//		res = f_close(&file);
+		f_sync(&file);
 
-		makeLog(&file, &bw, mail_status);
+		delay_ms(2000);
 	}
 
+	res = f_close(&file);
+	checkFRESULT(res);
+	res = f_mount(NULL, "", 0);
+	checkFRESULT(res);
 
-}
+	received_led_off();
+	debug_led_off();
+} /* End of main(). */
 
 FRESULT makeLog(FIL* file, UINT* bw, uint8_t mail_status) {
 	char buff[200];
@@ -125,10 +138,13 @@ FRESULT makeLog(FIL* file, UINT* bw, uint8_t mail_status) {
 	/* Timestamp + object presence in the mailbox. */
 	if(mail_status) {
 		sprintf((buff + lastPos), ",OBJECT PLACED IN MAILBOX\r\n");
+//		printf("Mail present...\r\n");
 	}
 	else {
 		sprintf((buff + lastPos), ",OBJECT TAKEN OUT OF MAILBOX\r\n");
+//		printf("No mail...\r\n");
 	}
+
 
 	return(f_write(file, buff, ((UINT)strlen((char*)buff)), bw));
 }
@@ -138,12 +154,48 @@ FRESULT makeLog(FIL* file, UINT* bw, uint8_t mail_status) {
 void checkFRESULT(FRESULT res) {
 	if(res != FR_OK) {
 		debug_led_on();
+//		printf("res != FR_OK...\r\n");
 	}
 	else {
 		debug_led_off();
+//		printf("res == FR_OK...\r\n");
 	}
 }
 
+
+/* Configure interrupt for user-button. */
+void configureButton(void) {
+	/* Disable system interrupt. */
+	__disable_irq();
+
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	/* Input mode.*/
+	GPIOA->MODER &= GPIO_MODER_MODER0_Msk;
+
+	/* Enable Pull-up. */
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPD0_0;
+
+	/* Configure interrupt for PA0 (User-button) to falling trigger. */
+	EXTI->IMR |= EXTI_IMR_MR0;
+	EXTI->FTSR |= EXTI_FTSR_TR0;
+
+	/* Set PA0 as source for the interrupt. */
+	SYSCFG->EXTICR[0] &= ~SYSCFG_EXTICR1_EXTI0_Msk;
+
+	__NVIC_EnableIRQ(EXTI0_IRQn);
+
+	/* Enable system. */
+	__enable_irq();
+}
+
+void EXTI0_IRQHandler(void) {
+	if(EXTI->PR & EXTI_PR_PR0) {
+		EXTI->PR |= EXTI_PR_PR0;
+		run = 0;
+	}
+}
 
 
 
--- mailbox_notification_transmitter/Src/main.c
+++ mailbox_notification_transmitter/Src/main.c
@@ -5,80 +5,124 @@
 
 #define DEBUG_MODE__
 #define ACTION_DURATION (6U)
+void configureButton(void);
 
+volatile uint8_t mail_status = 0x00;
 
 int main(void) {
-	uint8_t wakeFromStandby = check_standby_flag();
+	configureButton();
 	init_debug_mode();
+	while(1) {
 
-	/* Awaken from standy-by mode. */
-	if(wakeFromStandby) {
-		uint8_t isMailPresent;
-
-		/* Initial check to be compared after deep sleep. */
-		uint8_t init_mail_status = verify_mail_presence();
-		reset_low_power_mode_flags();
-
-#ifdef DEBUG_MODE__
-		/* For debug. */
-		for(volatile int i = 0; i < 2; ++i) {
-			debug_led_on();
-			delay_ms(500);
-			debug_led_off();
-			delay_ms(500);
-		}
-#endif
-		/* Go to deep sleep for 1 minute. Mail is either being placed inside or taken out of the mailbox. */
-		wait_for_action_done(ACTION_DURATION);
-		isMailPresent = verify_mail_presence();
-
-		/* Cases:
-		 * Case #1: If isMailPresent = 1 && init_mail_status = 0, then mail just got in the mailbox.
-		 * Case #2: If isMailPresent = 0 && init_mail_status = 1, then mail was taken out of the mailbox.
-		 * Case #3: If isMailPresent = 0 && init_mail_status = 0, then false alarm. No notification will be sent.
-		 * */
-		if(isMailPresent) { /* Case #1. */
-			debug_led_on();
-			send_notification(MAIL_PRESENT);
-		}
-		else { /* else, either false alarm or mail is taken out of the mailbox. */
-
-#ifdef DEBUG_MODE__
-			/* For debug. */
-			for(volatile int i = 0; i < 3; ++i) {
-				debug_led_on();
-				delay_ms(100);
-				debug_led_off();
-				delay_ms(100);
-			}
-#endif
-			if(init_mail_status == 1) { /* Case #2. */
-				send_notification(MAIL_ABSENT);
-			}
-			/* else, Case #3 just go to standby mode. */
-		}
 	}
-
-#ifdef DEBUG_MODE__
-	/* For debug. */
-	for(volatile int i = 0; i < 2; ++i) {
-		debug_led_on();
-		delay_ms(2000);
-	}
-#endif
-
-	/* Go to stand-by mode and wait for mailbox lid to trigger wake-up. */
-	set_standby_mode();
+//	uint8_t wakeFromStandby = check_standby_flag();
+//	init_debug_mode();
+//
+//	/* Awaken from standy-by mode. */
+//	if(wakeFromStandby) {
+//		uint8_t isMailPresent;
+//
+//		/* Initial check to be compared after deep sleep. */
+//		uint8_t init_mail_status = verify_mail_presence();
+//		reset_low_power_mode_flags();
+//
+//#ifdef DEBUG_MODE__
+//		/* For debug. */
+//		for(volatile int i = 0; i < 2; ++i) {
+//			debug_led_on();
+//			delay_ms(500);
+//			debug_led_off();
+//			delay_ms(500);
+//		}
+//#endif
+//		/* Go to deep sleep for 1 minute. Mail is either being placed inside or taken out of the mailbox. */
+//		wait_for_action_done(ACTION_DURATION);
+//		isMailPresent = verify_mail_presence();
+//
+//		/* Cases:
+//		 * Case #1: If isMailPresent = 1 && init_mail_status = 0, then mail just got in the mailbox.
+//		 * Case #2: If isMailPresent = 0 && init_mail_status = 1, then mail was taken out of the mailbox.
+//		 * Case #3: If isMailPresent = 0 && init_mail_status = 0, then false alarm. No notification will be sent.
+//		 * */
+//		if(isMailPresent) { /* Case #1. */
+//			debug_led_on();
+//			send_notification(MAIL_PRESENT);
+//		}
+//		else { /* else, either false alarm or mail is taken out of the mailbox. */
+//
+//#ifdef DEBUG_MODE__
+//			/* For debug. */
+//			for(volatile int i = 0; i < 3; ++i) {
+//				debug_led_on();
+//				delay_ms(100);
+//				debug_led_off();
+//				delay_ms(100);
+//			}
+//#endif
+//			if(init_mail_status == 1) { /* Case #2. */
+//				send_notification(MAIL_ABSENT);
+//			}
+//			/* else, Case #3 just go to standby mode. */
+//		}
+//	}
+//
+//#ifdef DEBUG_MODE__
+//	/* For debug. */
+//	for(volatile int i = 0; i < 2; ++i) {
+//		debug_led_on();
+//		delay_ms(2000);
+//	}
+//#endif
+//
+//	/* Go to stand-by mode and wait for mailbox lid to trigger wake-up. */
+//	set_standby_mode();
 } /* End of main(). */
 
 
-void RTC_WKUP_IRQHandler(void) {
-	if(EXTI->PR & EXTI_PR_PR22) {
-		EXTI->PR |= EXTI_PR_PR22;
-		reset_low_power_mode_flags();
-		disable_wakeup_timer();
+//void RTC_WKUP_IRQHandler(void) {
+//	if(EXTI->PR & EXTI_PR_PR22) {
+//		EXTI->PR |= EXTI_PR_PR22;
+//		reset_low_power_mode_flags();
+//		disable_wakeup_timer();
+//	}
+//} /* End of RTC_WKUP_IRQHandler(). */
+
+
+/* Configure interrupt for user-button. */
+void configureButton(void) {
+	/* Disable system interrupt. */
+	__disable_irq();
+
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	/* Input mode.*/
+	GPIOA->MODER &= ~GPIO_MODER_MODER0_Msk;
+
+	/* Enable Pull-up. */
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPD0_0;
+
+	/* Configure interrupt for PA0 (User-button) to falling trigger. */
+	EXTI->IMR |= EXTI_IMR_MR0;
+	EXTI->FTSR |= EXTI_FTSR_TR0;
+
+	/* Set PA0 as source for the interrupt. */
+	SYSCFG->EXTICR[0] &= ~SYSCFG_EXTICR1_EXTI0_Msk;
+
+	__NVIC_EnableIRQ(EXTI0_IRQn);
+
+	/* Enable system. */
+	__enable_irq();
+}
+
+void EXTI0_IRQHandler(void) {
+	if(EXTI->PR & EXTI_PR_PR0) {
+		EXTI->PR |= EXTI_PR_PR0;
+		mail_status ^= mail_status;
+		toggle_led();
+		send_notification(mail_status);
 	}
-} /* End of RTC_WKUP_IRQHandler(). */
+}
 
 
 
